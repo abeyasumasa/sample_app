2e2a9d35ade042aae329cf9a61ee2924ded8ee8c
abeyasumasa <abeyasumasa@gmail.com>
2020-01-05 01:35:51 +0900

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  #アクセサリーメソッドを使用する
+  attr_accessor :remember_token
   #データベースに渡される直前に小文字に変換（コールバックメソッドを使う）
   before_save { email.downcase! }
   
@@ -21,4 +23,27 @@ class User < ApplicationRecord
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
 end
