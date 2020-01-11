f9fc45a7d4a308252b2f0343785e4aa065bb6023
abeyasumasa <abeyasumasa@gmail.com>
2020-01-11 16:49:01 +0900

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'https://agile-tundra-61846.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
